028adcbc95064489f5a965b1e2741a4b6099ecd6
Saliha Utami Rizkia <salihautami@hotmail.com>
2021-03-24 19:12:44 +0100

    updated rook functions

--- src/chessgui/pieces/Rook.java
+++ src/chessgui/pieces/Rook.java
@@ -6,18 +6,24 @@ public class Rook extends Piece {
 
     public Rook(int x, int y, boolean is_white, String file_path, Board board)
     {
-        super(x,y,is_white,file_path, board);
+        super(x,y,is_white,file_path, board); //reference
     }
     
     @Override
     public boolean canMove(int destination_x, int destination_y)
     {
-        // Remember: A rook can move as many squares as he wants either forward,
-        // backward, or sideways without jumping any pieces. He cannot attack
-        // his own pieces.
         
-                // WRITE CODE HERE
+        //return a piece if theres one at that place, return null if there isnt one
+        Piece possiblePiece = board.getPiece(destination_x, destination_y);
         
+        if (possiblePiece != null){
+            if (possiblePiece.isWhite() && this.isWhite()){
+                return false;
+            }
+            if (possiblePiece.isBlack() && this.isBlack()){
+                return false;
+            }
+        }
         
         return true;
     }
